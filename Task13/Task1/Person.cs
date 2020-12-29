b5e60651a23b2b52d6079c2f445069823cc0ab9e
kolbasator <76650533+kolbasator@users.noreply.github.com>
2020-12-29 15:58:50 +0600

    Add files via upload

---
+++ Task13/Task1/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    class Person
+    {
+        private string firstName;
+        private string familyName;
+        private string adress;
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"My name is \n{this.Name}\n{this.FamilyName}");
+        }
+        public void SetData(string name, string lastname)
+        {
+            this.familyName = lastname;
+            this.firstName = name;
+        }
+        public string Name
+        {
+            get
+            {
+                return firstName;
+            }
+
+
+        }
+        public string FamilyName
+        {
+            get
+            {
+                return familyName;
+            }
+        }
+        public string Adress
+        {
+            get
+            {
+                return Adress;
+            }
+            set
+            {
+                adress = value;
+            }
+        }
+    }
+}
