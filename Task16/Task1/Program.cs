f66dfa79570f344dc591b358d7c6e20c2a948574
kolbasator <76650533+kolbasator@users.noreply.github.com>
2020-12-29 16:01:23 +0600

    Add files via upload

---
+++ Task16/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Person me = new Person();
+            me.SetData("Musyaka", "Chebotarev",me.GetDataInPostalFormat);
+            me.IntroduceYourself();
+            Console.ReadLine();
+        }
+    }
+}
