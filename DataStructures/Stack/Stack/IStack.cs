11272fba53ac3f321da6c8b5d9b169fcbd4ac215
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-01-28 21:05:41 +0600

    stack

---
+++ DataStructures/Stack/Stack/IStack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public interface IStack<T>
+    {
+        int Count { get; }
+        bool IsEmpty { get; }
+        void Push(T entity);
+        T Pop();
+        T Peek();
+    }
+}
---
+++ DataStructures/Stack/Stack/StackViaArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class StackViaArray<T> : IStack<T>
+    {
+        public int Count { get; }
+        public bool IsEmpty { get; }
+
+        public T[] Items;
+
+        public StackViaArray(int length)
+        {
+            Items = new T[length];
+        }
+
+
+        public void Push(T entity)
+        {
+            if (Count == Items.Length)
+                throw new InvalidOperationException("Переполнение стека");
+            int index = Count;
+            Items[index++] = entity;
+        }
+        public T Peek()
+        {
+            return Items[Count - 1];
+        }
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Стек пуст");
+            int index = Count; 
+            T item = Items[index--];
+            Items[Count] = default(T);  
+            return item;
+        }
+
+    }
+}
