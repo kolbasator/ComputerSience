918012d8b7f4a452730c717608f807880e12b546
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-01-31 20:09:34 +0600

    task

---
+++ Codewars/LarioAndMuigiPipeProblem/LarioAndMuigiPipeProblem/Fixer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Fixer
+{
+    public static List<int> PipeFix(List<int> numbers)
+    {
+        List<int> fixedNumbers = new List<int>(numbers.Count);
+        int a = numbers[0];
+        int b = numbers[numbers.Count - 1];
+        int i = a;
+        while (i <= b)
+        {
+            fixedNumbers.Add(i);
+            i++;
+        }
+        return fixedNumbers;
+    }
+}
---
+++ Codewars/LarioAndMuigiPipeProblem/NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq; 
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        [Test]
+        public void FixThosePipes_t1()
+        {
+            Assert.AreEqual(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, Fixer.PipeFix(new List<int> { 1, 2, 3, 5, 6, 8, 9 }));
+        }
+
+        [Test]
+        public void FixThosePipes_t2()
+        {
+            Assert.AreEqual(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 }, Fixer.PipeFix(new List<int> { 1, 2, 3, 12 }));
+        }
+
+        [Test]
+        public void FixThosePipes_t3()
+        {
+            Assert.AreEqual(new List<int> { 6, 7, 8, 9 }, Fixer.PipeFix(new List<int> { 6, 9 }));
+        }
+    }
+}
