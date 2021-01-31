4384a7fdce26ffe673c21abc22fea8bfd7635483
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-01-31 12:48:01 +0600

    Queue with unit tests

---
+++ DataStructures/Queue/Queue/IQueue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    public  interface IQueue<T>
+    {
+        /// <summary>
+        /// Property verifies that queue is empty.
+        /// </summary>
+        bool IsEmpty { get; }
+
+        /// <summary>
+        /// Property verifies that queue is full.
+        /// </summary>
+        bool IsFull { get; }
+
+        /// <summary>
+        /// Returns the number of elements in queue
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Adds item to the end of the queue.
+        /// </summary>
+        void Enqueue(T entity);
+
+        /// <summary>
+        /// Pops item from the top of the queue and gives pointer to it.
+        /// </summary>
+        T Dequeue();
+
+        /// <summary>
+        /// Returns the top element of the queue without removing from queue.
+        /// </summary>
+        T Peek();
+    }
+}
---
+++ DataStructures/Queue/Queue/QueueViaArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queue
+{
+    public class QueueViaArray<T> : IQueue<T>
+    {
+        public int Count { get; private set; }
+
+        private int _head;
+
+        private int _tail;
+
+        public bool IsEmpty => Count == 0;
+
+        private T[] _items;
+
+        public bool IsFull => Count == _items.Length;
+
+        public QueueViaArray(int length)
+        {
+            if (length < 0)
+                throw new InvalidOperationException("Invalid length.");
+            _items = new T[length];
+            _head = 0;
+            _tail = 0;
+        }
+        public T Peek()
+        {
+            if (IsEmpty)
+                throw new IndexOutOfRangeException("Queue is empty.");
+            return _items[_head];
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty)
+                throw new IndexOutOfRangeException("Queue is empty.");
+            T entity = _items[_head];
+            _items[_head] = default;
+            _head = (_head + 1) % _items.Length;
+            Count--;
+            return entity;
+        }
+
+        public void Enqueue(T entity)
+        {
+            if (IsFull)
+                throw new IndexOutOfRangeException("Queue is full.");
+            _items[_tail] = entity;
+            _tail = (_tail + 1) % _items.Length;
+            Count++;
+
+        }
+
+    }
+}
